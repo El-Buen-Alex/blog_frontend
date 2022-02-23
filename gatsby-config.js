d0025a71f3d5d7d8521fb39df9f8225700da3248
Daniel Pincay <alexispincay005@gmail.com>
2022-02-23 16:24:08 -0500

    cambio de api de obtencion de datos

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: "https://myblogconstrapi.herokuapp.com",
         collectionTypes: ["articulo"],
         queryLimit: 1000,
       },
